80cc7d9167592a6aa811767d34f3cb9e15351d4d
David Farkas <david.farkas93@gmail.com>
2016-06-29 18:52:04 +0200

    First version of location triangulation

--- src/main/java/hu/elte/ik/robotika/futar/vertx/backend/verticle/HTTPVerticle.java
+++ src/main/java/hu/elte/ik/robotika/futar/vertx/backend/verticle/HTTPVerticle.java
@@ -49,15 +49,97 @@ public class HTTPVerticle extends AbstractVerticle {
 	private final Logger log = LoggerFactory.getLogger(HTTPVerticle.class);
 	private Map<String, ServerWebSocket> sockets = new LinkedHashMap<String, ServerWebSocket>();
 
-	private Map<String, Point> placedBTDevices = new LinkedHashMap<String, Point>();
+	private Map<String, JsonObject> placedBTDevices = new LinkedHashMap<String, JsonObject>();
 
-	private List<JsonObject> newBTDevices = new ArrayList<JsonObject>();
+	private Map<String, JsonObject> newBTDevices = new LinkedHashMap<String, JsonObject>();
 
-	private Map<String, Map<String, Integer>> btData = new LinkedHashMap<String, Map<String, Integer>>();
+	private Map<String, Map<String, Double>> btData = new LinkedHashMap<String, Map<String, Double>>();
 
 	private List<String> activeRobots = new ArrayList<String>();
 
 	private String webRoot;
+
+	private static final double EPSILON = 0.000001;
+
+	private boolean calculateThreeCircleIntersection(double x0, double y0, double r0,
+                                                 double x1, double y1, double r1,
+                                                 double x2, double y2, double r2)
+	{
+	    double a, dx, dy, d, h, rx, ry;
+	    double point2_x, point2_y;
+
+	    /* dx and dy are the vertical and horizontal distances between
+	    * the circle centers.
+	    */
+	    dx = x1 - x0;
+	    dy = y1 - y0;
+
+	    /* Determine the straight-line distance between the centers. */
+	    d = Math.sqrt((dy*dy) + (dx*dx));
+
+	    /* Check for solvability. */
+	    if (d > (r0 + r1))
+	    {
+	        /* no solution. circles do not intersect. */
+	        return false;
+	    }
+	    if (d < Math.abs(r0 - r1))
+	    {
+	        /* no solution. one circle is contained in the other */
+	        return false;
+	    }
+
+	    /* 'point 2' is the point where the line through the circle
+	    * intersection points crosses the line between the circle
+	    * centers.
+	    */
+
+	    /* Determine the distance from point 0 to point 2. */
+	    a = ((r0*r0) - (r1*r1) + (d*d)) / (2.0 * d) ;
+
+	    /* Determine the coordinates of point 2. */
+	    point2_x = x0 + (dx * a/d);
+	    point2_y = y0 + (dy * a/d);
+
+	    /* Determine the distance from point 2 to either of the
+	    * intersection points.
+	    */
+	    h = Math.sqrt((r0*r0) - (a*a));
+
+	    /* Now determine the offsets of the intersection points from
+	    * point 2.
+	    */
+	    rx = -dy * (h/d);
+	    ry = dx * (h/d);
+
+	    /* Determine the absolute intersection points. */
+	    double intersectionPoint1_x = point2_x + rx;
+	    double intersectionPoint2_x = point2_x - rx;
+	    double intersectionPoint1_y = point2_y + ry;
+	    double intersectionPoint2_y = point2_y - ry;
+
+	    log.info("INTERSECTION Circle1 AND Circle2:", "(" + intersectionPoint1_x + "," + intersectionPoint1_y + ")" + " AND (" + intersectionPoint2_x + "," + intersectionPoint2_y + ")");
+
+	    /* Lets determine if circle 3 intersects at either of the above intersection points. */
+	    dx = intersectionPoint1_x - x2;
+	    dy = intersectionPoint1_y - y2;
+	    double d1 = Math.sqrt((dy*dy) + (dx*dx));
+
+	    dx = intersectionPoint2_x - x2;
+	    dy = intersectionPoint2_y - y2;
+	    double d2 = Math.sqrt((dy*dy) + (dx*dx));
+
+	    if(Math.abs(d1 - r2) < EPSILON) {
+	        log.info("INTERSECTION Circle1 AND Circle2 AND Circle3:", "(" + intersectionPoint1_x + "," + intersectionPoint1_y + ")");
+	    }
+	    else if(Math.abs(d2 - r2) < EPSILON) {
+	        log.info("INTERSECTION Circle1 AND Circle2 AND Circle3:", "(" + intersectionPoint2_x + "," + intersectionPoint2_y + ")"); //here was an error
+	    }
+	    else {
+	        log.info("INTERSECTION Circle1 AND Circle2 AND Circle3:", "NONE");
+	    }
+	    return true;
+	}
 
 	// Create some static test entity user
 	private static Map<Integer, User> users = new LinkedHashMap<>();
@@ -75,9 +157,11 @@ public class HTTPVerticle extends AbstractVerticle {
             .addOutboundPermitted(new PermittedOptions().setAddressRegex("new.robot"));
 		options.addOutboundPermitted(new PermittedOptions().setAddressRegex("logout.robot"));
 		options.addOutboundPermitted(new PermittedOptions().setAddressRegex("new.bt.device"));
+		options.addOutboundPermitted(new PermittedOptions().setAddressRegex("placed.bt.device"));
 		options.addInboundPermitted(new PermittedOptions().setAddressRegex("login.robot"));
 		options.addInboundPermitted(new PermittedOptions().setAddressRegex("login.client"));
 		options.addInboundPermitted(new PermittedOptions().setAddressRegex("robot.\\.[0-9]+"));
+		options.addInboundPermitted(new PermittedOptions().setAddressRegex("place.bt.device"));
 		// use this to send message to the clients:
 		// routingContext.vertx().eventBus().publish("muhaha", routingContext.getBodyAsString());
     return SockJSHandler.create(vertx).bridge(options, event -> {
@@ -100,7 +184,7 @@ public class HTTPVerticle extends AbstractVerticle {
 						log.info(event.rawMessage());
  	        }
 					if (event.type() == BridgeEventType.SEND) {
-						log.info("Client sent a message");
+						log.info("Client sent a message: " + event.rawMessage());
 						if (event.rawMessage().getString("address").equals("login.robot") && !activeRobots.contains(event.socket().writeHandlerID()))
 						{
 							log.info("Robot logged in");
@@ -121,6 +205,25 @@ public class HTTPVerticle extends AbstractVerticle {
 								response.put("robotId", key);
 								vertx.eventBus().publish("new.robot", Json.encode(response));
 							}
+
+							for (JsonObject value : placedBTDevices.values()) {
+							    vertx.eventBus().publish("placed.bt.device", Json.encode(value));
+							}
+
+							for (JsonObject value : newBTDevices.values()) {
+							    vertx.eventBus().publish("new.bt.device", Json.encode(value));
+							}
+						}
+
+						if (event.rawMessage().getString("address").equals("place.bt.device"))
+						{
+
+							 JsonObject data = event.rawMessage().getJsonObject("body");
+							 log.info("Place bt device: " + data.getString("address"));
+							 placedBTDevices.remove(data.getString("address"));
+							 placedBTDevices.put(data.getString("address"), data);
+							 newBTDevices.remove(data.getString("address"));
+							 vertx.eventBus().publish("placed.bt.device", Json.encode(data));
 						}
  	        }
 	        event.complete(true);
@@ -213,21 +316,75 @@ public class HTTPVerticle extends AbstractVerticle {
 										log.info("found.bluetooth");
 										JsonObject pb = response.getJsonObject("data");
 										if (placedBTDevices.get(pb.getString("address")) == null &&
-												!newBTDevices.contains(pb))
+												newBTDevices.get(pb.getString("address")) == null)
 												{
-													newBTDevices.add(pb);
+													JsonObject data = new JsonObject(Json.encode(pb));
+													data.remove("rssi");
+													newBTDevices.put(pb.getString("address"), data);
 													log.info("New bt device: " + buffer);
-													vertx.eventBus().publish("new.bt.device", Json.encode(pb));
+													vertx.eventBus().publish("new.bt.device", Json.encode(data));
 												}
 
-										if (btData.get(id) == null)
-										{
-											btData.put(id, new LinkedHashMap<String, Integer>());
-											log.info("New bt data for the following robot: " + id);
-										}
 											btData.get(id).remove(pb.getString("address"));
-											btData.get(id).put(pb.getString("address"), pb.getInteger("rssi"));
+
 											log.info("Update bt data: " + id + " " + pb.getString("address") + " " + pb.getInteger("rssi"));
+											double d = Math.pow(10,(pb.getInteger("rssi") - (-40))/((-10)*2));
+											//RSSI (dBm) = -10n log10(d) + A
+											log.info("the calculated distance is around: " + d + "m");
+											btData.get(id).put(pb.getString("address"), d*27);
+							} else if (response.getString("action") != null &&
+									response.getString("action").equals("start.bluetooth.scan") &&
+									sockets.get(id) == null)
+							{
+								log.info("start.bluetooth.scan");
+								btData.remove(id);
+								btData.put(id, new LinkedHashMap<String, Double>());
+
+							}
+							else if (response.getString("action") != null &&
+									response.getString("action").equals("finished.bluetooth.scan") &&
+									sockets.get(id) == null)
+							{
+								log.info("finished.bluetooth.scan");
+								if (btData.get(id).size() >= 3)
+								{
+									double x0 = 0, y0 = 0, r0 = 0, x1 = 0, y1 = 0, r1 = 0, x2 = 0, y2 = 0, r2 = 0;
+									int i = 0;
+									for (Map.Entry<String, Double> entry : btData.get(id).entrySet()) {
+									    String key = entry.getKey();
+											JsonObject placedBT = placedBTDevices.get(key);
+											if (placedBT == null)
+											{
+												continue;
+											}
+									    double value = entry.getValue();
+											if (i == 0) {
+												x0 = placedBT.getDouble("x");
+												y0 = placedBT.getDouble("y");
+												r0 = value;
+											} else if (i == 1) {
+												x1 = placedBT.getDouble("x");
+												y1 = placedBT.getDouble("y");
+												r1 = value;
+											} else if (i == 2) {
+												x2 = placedBT.getDouble("x");
+												y2 = placedBT.getDouble("y");
+												r2 = value;
+											} else {
+												break;
+											}
+											i++;
+									}
+
+									if (i == 3) {
+										calculateThreeCircleIntersection(x0, y0, r0, x1, y1, r1, x2, y2, r2);
+									}
+
+
+								} else
+								{
+									log.info("There is not enough BT data to calculate the location of the robot.");
+								}
 							}
 
 							log.info("got the following message from " + id + ": " + Json.encode(response));
